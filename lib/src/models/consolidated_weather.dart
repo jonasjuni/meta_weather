52bc0cf1c375e87da6a45919c218c79625aab545
Jonas Cerqueira da Silva Junior <jonas.c@live.com>
2021-09-26 19:59:30 -0300

    complete json parse

--- lib/src/models/consolidated_weather.dart
+++ lib/src/models/consolidated_weather.dart
@@ -2,7 +2,8 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'consolidated_weather.g.dart';
 
-enum WeatherCondition {
+/// Weather state enum
+enum WeatherState {
   @JsonValue('sn')
   snow,
   @JsonValue('sl')
@@ -23,27 +24,75 @@ enum WeatherCondition {
   lightCloud,
   @JsonValue('c')
   clear,
-  unknown
+  unknownEntry,
 }
 
-extension WeatherStateX on WeatherCondition {
-  String? get abbr => _$WeatherConditionEnumMap[this];
+extension WeatherStateX on WeatherState {
+  String? get abbr => _$WeatherStateEnumMap[this];
 }
 
 @JsonSerializable()
 class ConsolidatedWeather {
-  @JsonKey(
-      name: 'weather_state_abbr', unknownEnumValue: WeatherCondition.unknown)
-  final WeatherCondition condition;
+  /// Internal identifier for the forecast
+  final int id;
+
+  /// Text description of the weather state
   final String weatherStateName;
-  final double minTemp;
-  final double maxTemp;
-  @JsonKey(name: 'the_temp')
-  final double temp;
+
+  /// Weather state enum, one or two letter abbreviation of the weather state
+  @JsonKey(
+      name: 'weather_state_abbr', unknownEnumValue: WeatherState.unknownEntry)
+  final WeatherState weatherState;
+
+  /// Compass point of the wind direction
+  final String windDirectionCompass;
+
+  /// Date of entry creation
   final DateTime created;
 
-  ConsolidatedWeather(this.condition, this.weatherStateName, this.minTemp,
-      this.maxTemp, this.temp, this.created);
+  /// Date that the forecast or observation pertains to
+  final DateTime applicableDate;
+
+  /// Min temperature in `centigrade`
+  final double minTemp;
+
+  /// Max temperature in `centigrade`
+  final double maxTemp;
+
+  /// Current temperature in `centigrade`
+  @JsonKey(name: 'the_temp')
+  final double temp;
+
+  /// Wind speed in `mph`
+  final double windSpeed;
+
+  /// airPressue in `mbar`
+  final double airPressure;
+
+  /// percentage of water vapor in the air
+  final double humidity;
+
+  /// Visibility in `miles`
+  final double visibility;
+
+  /// Percentage of our interpretation of the level to which the forecasters agree with each other - 100% being a complete consensus.
+  final int predictability;
+
+  ConsolidatedWeather(
+      this.id,
+      this.weatherStateName,
+      this.weatherState,
+      this.windDirectionCompass,
+      this.created,
+      this.applicableDate,
+      this.minTemp,
+      this.maxTemp,
+      this.temp,
+      this.windSpeed,
+      this.airPressure,
+      this.humidity,
+      this.visibility,
+      this.predictability);
 
   factory ConsolidatedWeather.fromJson(Map<String, dynamic> json) =>
       _$ConsolidatedWeatherFromJson(json);
--- lib/src/models/consolidated_weather.g.dart
+++ lib/src/models/consolidated_weather.g.dart
@@ -9,36 +9,57 @@ part of 'consolidated_weather.dart';
 ConsolidatedWeather _$ConsolidatedWeatherFromJson(Map<String, dynamic> json) {
   return $checkedNew('ConsolidatedWeather', json, () {
     final val = ConsolidatedWeather(
+      $checkedConvert(json, 'id', (v) => v as int),
+      $checkedConvert(json, 'weather_state_name', (v) => v as String),
       $checkedConvert(
           json,
           'weather_state_abbr',
-          (v) => _$enumDecode(_$WeatherConditionEnumMap, v,
-              unknownValue: WeatherCondition.unknown)),
-      $checkedConvert(json, 'weather_state_name', (v) => v as String),
+          (v) => _$enumDecode(_$WeatherStateEnumMap, v,
+              unknownValue: WeatherState.unknownEntry)),
+      $checkedConvert(json, 'wind_direction_compass', (v) => v as String),
+      $checkedConvert(json, 'created', (v) => DateTime.parse(v as String)),
+      $checkedConvert(
+          json, 'applicable_date', (v) => DateTime.parse(v as String)),
       $checkedConvert(json, 'min_temp', (v) => (v as num).toDouble()),
       $checkedConvert(json, 'max_temp', (v) => (v as num).toDouble()),
       $checkedConvert(json, 'the_temp', (v) => (v as num).toDouble()),
-      $checkedConvert(json, 'created', (v) => DateTime.parse(v as String)),
+      $checkedConvert(json, 'wind_speed', (v) => (v as num).toDouble()),
+      $checkedConvert(json, 'air_pressure', (v) => (v as num).toDouble()),
+      $checkedConvert(json, 'humidity', (v) => (v as num).toDouble()),
+      $checkedConvert(json, 'visibility', (v) => (v as num).toDouble()),
+      $checkedConvert(json, 'predictability', (v) => v as int),
     );
     return val;
   }, fieldKeyMap: const {
-    'condition': 'weather_state_abbr',
     'weatherStateName': 'weather_state_name',
+    'weatherState': 'weather_state_abbr',
+    'windDirectionCompass': 'wind_direction_compass',
+    'applicableDate': 'applicable_date',
     'minTemp': 'min_temp',
     'maxTemp': 'max_temp',
-    'temp': 'the_temp'
+    'temp': 'the_temp',
+    'windSpeed': 'wind_speed',
+    'airPressure': 'air_pressure'
   });
 }
 
 Map<String, dynamic> _$ConsolidatedWeatherToJson(
         ConsolidatedWeather instance) =>
     <String, dynamic>{
-      'weather_state_abbr': _$WeatherConditionEnumMap[instance.condition],
+      'id': instance.id,
       'weather_state_name': instance.weatherStateName,
+      'weather_state_abbr': _$WeatherStateEnumMap[instance.weatherState],
+      'wind_direction_compass': instance.windDirectionCompass,
+      'created': instance.created.toIso8601String(),
+      'applicable_date': instance.applicableDate.toIso8601String(),
       'min_temp': instance.minTemp,
       'max_temp': instance.maxTemp,
       'the_temp': instance.temp,
-      'created': instance.created.toIso8601String(),
+      'wind_speed': instance.windSpeed,
+      'air_pressure': instance.airPressure,
+      'humidity': instance.humidity,
+      'visibility': instance.visibility,
+      'predictability': instance.predictability,
     };
 
 K _$enumDecode<K, V>(
@@ -67,16 +88,16 @@ K _$enumDecode<K, V>(
   ).key;
 }
 
-const _$WeatherConditionEnumMap = {
-  WeatherCondition.snow: 'sn',
-  WeatherCondition.sleet: 'sl',
-  WeatherCondition.hail: 'h',
-  WeatherCondition.thunderstorm: 't',
-  WeatherCondition.heavyRain: 'hr',
-  WeatherCondition.lightRain: 'lr',
-  WeatherCondition.showers: 's',
-  WeatherCondition.heavyCloud: 'hc',
-  WeatherCondition.lightCloud: 'lc',
-  WeatherCondition.clear: 'c',
-  WeatherCondition.unknown: 'unknown',
+const _$WeatherStateEnumMap = {
+  WeatherState.snow: 'sn',
+  WeatherState.sleet: 'sl',
+  WeatherState.hail: 'h',
+  WeatherState.thunderstorm: 't',
+  WeatherState.heavyRain: 'hr',
+  WeatherState.lightRain: 'lr',
+  WeatherState.showers: 's',
+  WeatherState.heavyCloud: 'hc',
+  WeatherState.lightCloud: 'lc',
+  WeatherState.clear: 'c',
+  WeatherState.unknownEntry: 'unknownEntry',
 };
